731c48acd4490bbd2e07b72cded057d4912571c8
Martinez <jdmartinez14@cps.k12.il.us>
2025-02-10 09:18:44 -0600

    https://github.com/JazminMrtz/python_learning

--- week_10/while_for_loops.py
+++ week_10/while_for_loops.py
@@ -1,79 +1,79 @@
-# ------------------------WHILE LOOPS-------------------------------------
-# # while loop = execute some code WHILE some condition remains true
+------------------------WHILE LOOPS-------------------------------------
+# while loop = execute some code WHILE some condition remains true
 
-# name = input("Enter your name:")
-# # iteration (noun version) = loop over something (verb form)
-# while name == "":
-#     print("You did not enter your name")
-#     name = input("Enter your name")
-# # infinite loops are bad
-# print(f"Hello {name}")
+name = input("Enter your name:")
+# iteration (noun version) = loop over something (verb form)
+while name == "":
+    print("You did not enter your name")
+    name = input("Enter your name")
+# infinite loops are bad
+print(f"Hello {name}")
 
-# age = int(input("Enter your age: "))
+age = int(input("Enter your age: "))
 
-# while age < 0:
-#     print("Age can't be negative:")
-#     age = int(input("Enter your age: "))
+while age < 0:
+    print("Age can't be negative:")
+    age = int(input("Enter your age: "))
 
-# print(f"You are {age} years old")
+print(f"You are {age} years old")
 
-# food = input("Enter a food you like (q to quit): ")
+food = input("Enter a food you like (q to quit): ")
 
-# while not food == "q":
-#     print(f"You like {food}")
-#     food = input("Enter another you like(q to quit): ")
-# print("Bye!")
+while not food == "q":
+    print(f"You like {food}")
+    food = input("Enter another you like(q to quit): ")
+print("Bye!")
 
-# num = input("Enter a # between 1-10: ")
+num = input("Enter a # between 1-10: ")
 
-# while num < 1 or num > 10:
-#     print(f"{num} is not valid")
-#     num = int(input("Enter a # between 1 - 10: "))
+while num < 1 or num > 10:
+    print(f"{num} is not valid")
+    num = int(input("Enter a # between 1 - 10: "))
 
-# print(f"Your number is {num}")
+print(f"Your number is {num}")
 
 # -----------------------FOR LOOPS-----------------------------------
 # for loops = execute a block of code a fixed numger of times
 #             you can iterate over a range, string, sequence, etc.
 # iterable = more than one thing / multiple
 # ------------------range-----------------------------
-# for x in range(1, 11, 3):
-#     print(x)
+for x in range(1, 11, 3):
+    print(x)
 
 # ------------------string------------------------------
-# credit_card = "1234-5678-9012-3456"
-# for x in credit_card:
-#     print(x) #prints one number each time you print
+credit_card = "1234-5678-9012-3456"
+for x in credit_card:
+    print(x) #prints one number each time you print
 
 # -------------------range------------------------------
-# for x in range(1, 21):
-#     if x == 13:
-#         continue #skips over
-#     else:
-#         print(x)
+for x in range(1, 21):
+    if x == 13:
+        continue #skips over
+    else:
+        print(x)
 
-# for x in range(1, 21):
-#     if x == 13:
-#         break #stops
-#     else: 
-#         print(x)
+for x in range(1, 21):
+    if x == 13:
+        break #stops
+    else: 
+        print(x)
 # ----------------------------------------------------------------------------------------------
 # create a list of famous movie characters
-# horror_characters = ["Freddy Krueger","Jason Voorhees","Micheal Myers", "Pennywise", "Chucky"]
-# # iterate through the list and print each character
+horror_characters = ["Freddy Krueger","Jason Voorhees","Micheal Myers", "Pennywise", "Chucky"]
+# iterate through the list and print each character
 
-# for character in horror_characters:
-#     # print(character)
-#     # if character == "Jason Voorhees":
-#     #     continue
-#     # print(character)
-#     # if character == "Micheal Myers":
-#     #     character = "Dracula" #swaps values
-#     #     continue
-#     # print(character)
-#     if character == "Pennywise":
-#         character = "Reeper"
-#         print(character)
+for character in horror_characters:
+    # print(character)
+    # if character == "Jason Voorhees":
+    #     continue
+    # print(character)
+    # if character == "Micheal Myers":
+    #     character = "Dracula" #swaps values
+    #     continue
+    # print(character)
+    if character == "Pennywise":
+        character = "Reeper"
+        print(character)
 
 # ------------------------------------------------------------------------------------------------
 # create a list of famous horror movies or your 
